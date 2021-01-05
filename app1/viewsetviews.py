519561d5cffd224650985907baf4239a37d3bc64
Vigneshwaran <vigneshwaran@vgts.tech>
2021-01-05 14:11:49 +0530

    initial commit

---
+++ app1/viewsetviews.py
@@ -0,0 +1,10 @@
+from django.http import Http404
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Employee
+from .serializers import EmployeeSerializer
+
+class employeeViewSet(viewsets.viewSet):
+    
